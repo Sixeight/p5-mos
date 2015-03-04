6bbe2ea93fe70f8399b8827ec61f6b93877860b4
Tomohiro Nishimura <tomohiro68@gmail.com>
2015-03-04 20:32:24 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Mos - It's new $module
+
+# SYNOPSIS
+
+    use Mos;
+
+# DESCRIPTION
+
+Mos is ...
+
+# LICENSE
+
+Copyright (C) Tomohiro Nishimura.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Tomohiro Nishimura <tomohiro68@gmail.com>
---
+++ lib/Mos.pm
@@ -0,0 +1,39 @@
+package Mos;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Mos - It's new $module
+
+=head1 SYNOPSIS
+
+    use Mos;
+
+=head1 DESCRIPTION
+
+Mos is ...
+
+=head1 LICENSE
+
+Copyright (C) Tomohiro Nishimura.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Tomohiro Nishimura E<lt>tomohiro68@gmail.comE<gt>
+
+=cut
+
