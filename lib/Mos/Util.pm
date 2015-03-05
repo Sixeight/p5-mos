69bf6cb00ad9b1466a79f9bf4930cae9b645d1ce
Tomohiro Nishimura <tomohiro68@gmail.com>
2015-03-06 01:30:19 +0900

    Fix indent

--- lib/Mos/Util.pm
+++ lib/Mos/Util.pm
@@ -9,24 +9,24 @@ use DateTime::Format::MySQL;
 our $TIME_ZONE = "UTC";
 
 sub now () {
-    my $now = DateTime->now(time_zone => $Mos::Util::TIME_ZONE);
-    $now->set_formatter( DateTime::Format::MySQL->new );
-    $now;
+  my $now = DateTime->now(time_zone => $Mos::Util::TIME_ZONE);
+  $now->set_formatter( DateTime::Format::MySQL->new );
+  $now;
 }
 
 sub time_string_from_datetime ($) {
-    my $dt = shift;
-    $dt->set_time_zone($Mos::Util::TIME_ZONE);
-    $dt->set_formatter(DateTime::Format::MySQL->new);
-    "" . $dt;
+  my $dt = shift;
+  $dt->set_time_zone($Mos::Util::TIME_ZONE);
+  $dt->set_formatter(DateTime::Format::MySQL->new);
+  "" . $dt;
 }
 
 sub datetime_from_time_string ($) {
-    my $ts = shift;
-    my $dt = DateTime::Format::MySQL->parse_datetime($ts);
-    $dt->set_time_zone($Mos::Util::TIME_ZONE);
-    $dt->set_formatter(DateTime::Format::MySQL->new);
-    $dt;
+  my $ts = shift;
+  my $dt = DateTime::Format::MySQL->parse_datetime($ts);
+  $dt->set_time_zone($Mos::Util::TIME_ZONE);
+  $dt->set_formatter(DateTime::Format::MySQL->new);
+  $dt;
 }
 
 1;
