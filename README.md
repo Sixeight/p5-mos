5c3fbb04c3dbb613306b19f0ae0cbd14b5d75fd4
Tomohiro Nishimura <tomohiro68@gmail.com>
2015-03-06 03:00:12 +0900

    Update documentation

--- README.md
+++ README.md
@@ -1,14 +1,49 @@
+
 # NAME
 
-Mos - It's new $module
+Mos - Simple O/R mapper for my Perl practice.
 
 # SYNOPSIS
 
-    use Mos;
+    package Service::User;
+    use parent "Mos::Service";
+
+    package Model::User;
+    use parent "Mos::Model";
+    use Mos::Model;
+
+    column("id", "int");
+    column("name", "string");
+    timestamp;
+
+    package main;
+
+    Service::User->connect("dbi:SQLite:dbname=:memory");
+    Service::User->create({id => 1, name => "bob"});
+    Service::User->create({id => 2, name => "alice"});
+
+    my $users = Service::User->all;
+
+    my $user = Service::User->find(1);
+    print $user->name;
+
+    my $alice = Service::User->find({name => "alice"});
+    print $alice->created_at;
+
+    $alice->name("Mrs. Alice");
+    Service::User->update($alice);
 
 # DESCRIPTION
 
-Mos is ...
+Mos is very simple O/R mapper for my Perl practice.
+That means this module is not for production.
+
+Please **DO NOT USE** on production environment.
+
+# TODO
+
+- Transaction
+- Documentation
 
 # LICENSE
 
--- lib/Mos.pm
+++ lib/Mos.pm
@@ -14,15 +14,58 @@ __END__
 
 =head1 NAME
 
-Mos - It's new $module
+Mos - Simple O/R mapper for my Perl practice.
 
 =head1 SYNOPSIS
 
-    use Mos;
+    package Service::User;
+    use parent "Mos::Service";
+
+    package Model::User;
+    use parent "Mos::Model";
+    use Mos::Model;
+
+    column("id", "int");
+    column("name", "string");
+    timestamp;
+
+    package main;
+
+    Service::User->connect("dbi:SQLite:dbname=:memory");
+    Service::User->create({id => 1, name => "bob"});
+    Service::User->create({id => 2, name => "alice"});
+
+    my $users = Service::User->all;
+
+    my $user = Service::User->find(1);
+    print $user->name;
+
+    my $alice = Service::User->find({name => "alice"});
+    print $alice->created_at;
+
+    $alice->name("Mrs. Alice");
+    Service::User->update($alice);
 
 =head1 DESCRIPTION
 
-Mos is ...
+Mos is very simple O/R mapper for my Perl practice.
+That means this module is not for production.
+
+Please B<DO NOT USE> on production environment.
+
+=head1 TODO
+
+=over
+
+=item
+
+Transaction
+
+=item
+
+Documentation
+
+=back
 
 =head1 LICENSE
 
@@ -36,4 +79,3 @@ it under the same terms as Perl itself.
 Tomohiro Nishimura E<lt>tomohiro68@gmail.comE<gt>
 
 =cut
-
